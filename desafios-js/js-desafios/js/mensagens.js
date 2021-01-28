2246f2ac38297a4130aeb0fd86cc326a8fe36e17
Isaac Daniel <isaacinfo2017@gmail.com>
2021-01-28 12:20:15 -0300

    Deletes

--- desafios-js/js-desafios/js/mensagens.js
+++
@@ -1,9 +0,0 @@
-function apertarBotao01(){
-    window.alert('Você clicou no 1º botão!')     
-}
-function apertarBotao02(){
-    window.alert('Você clicou no 2º botão!')
-}
-function apertarBotao03(){
-    window.alert('Você clicou no 3º botão!')
-}
