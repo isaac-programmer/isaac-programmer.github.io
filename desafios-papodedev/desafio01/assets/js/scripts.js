c124371fc57183785846c2d3a986b819b823f027
Isaac Daniel <isaacinfo2017@gmail.com>
2021-02-13 11:29:43 -0300

    Updates

---
+++ desafios-papodedev/desafio01/assets/js/scripts.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.tenis')
+function mudarParaVerde(){
+    img.setAttribute('src', 'assets/images/tenis-verde.png')
+}
+function mudarParaVermelho(){
+    img.setAttribute('src', 'assets/images/tenis-vermelho.png')
+}
+function mudarParaAzul(){
+    img.setAttribute('src', 'assets/images/tenis-azul.png')
+}
